2aaedd79a31ede71d6792808b34c26763b4a1d5d
Benamin <1282546003@qq.com>
2021-01-25 14:10:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -11,7 +11,12 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './assets/style.scss'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI, {size: 'small', zIndex: 3000});
+
+
+//全局修改默认配置，
+Vue.use(ElementUI, {size: 'small', zIndex: 3000}); //样式的size
+ElementUI.Dialog.props.closeOnClickModal.default = false; //点击空白处不能关闭弹窗
+
 
 Vue.component('footer-copyright', {
   template: '<p class="footer-msg">©CopyRight 2021 汇纳科技 版权所有</p>'
